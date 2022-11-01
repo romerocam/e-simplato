3518afc07a2f759ddc6402935ecf11d173efc6ea
romerocam <romerocam@hotmail.com>
2022-11-01 13:40:30 -0300

    images folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary" >
+        {/* desktop/tablet */}
+        <div  className="hidden md:flex w-full h-full items-center justify-between">
+            
+        </div>
+        {/* mobile */}
+        <div className="flex items-center justify-between md:hidden w-full h-full ">
+
+        </div>
+    </div>
   )
 }
 
