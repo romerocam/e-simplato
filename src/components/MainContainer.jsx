9db612f614caecdaefdcb155ad9e68eb383f9c46
romerocam <romerocam@hotmail.com>
2022-11-02 09:44:39 -0300

    main container styling

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Delivery from "../img/delivery.png";
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center">
+        <div>
+          <p>Delivery</p>
+          <div className="w-10 h-10 rounded-full overflow-hidden">
+            <img src={Delivery} className='w-full h-full object-contain' alt="delivery" />
+          </div>
+        </div>
+      </div>
+      <div className="py-2 flex-1"></div>
+    </div>
+  );
+};
 
-export default MainContainer
+export default MainContainer;
